784e8b9b8c3a6267f34e750abc2d90fc5392fbf5
KChasteSun <kchastes@outlook.com>
2022-09-15 22:53:50 +0800

    study

---
+++ src/test/java/org/apache/self/entity/MyInterceptor2.java
@@ -0,0 +1,25 @@
+package org.apache.self.entity;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+
+@Intercepts({@Signature(
+  type= Executor.class,
+  method = "query",
+  args = {MappedStatement.class,Object.class, RowBounds.class, ResultHandler.class})})
+public class MyInterceptor2 implements Interceptor {
+
+
+  @Override
+  public Object intercept(Invocation invocation) throws Throwable {
+    System.out.println(2);
+    return invocation.proceed();
+  }
+}
